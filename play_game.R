b310675259cdadb23d8669ca1ae8a4ec2f5d7364
jimpleussjr <jim.pleuss@gmail.com>
2022-01-22 15:39:08 +0300

    allow for initial probabilities

--- play_game.R
+++ play_game.R
@@ -1,13 +1,16 @@
 source("wordle.R")
 
-reset()
+reset()  
 prep_words()
+
+# For the first round of words
+get_words(get_filtered_words())
 
 ### write the word you entered in "letters"
 ### and the color of the results in "colors" where:
 ###      green = 'g', black = 'b', yellow = 'y'
 run_attempt(letters_attempt = "wench", colors_attempt = "byggb")
 
-
+# Rerun it for subsequent attempts. If you need to start over, just run reset()
 
 
