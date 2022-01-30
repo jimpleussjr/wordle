6cb4bc8a3625dc97895a73e6032e734d7408aec5
jimpleussjr <jim.pleuss@gmail.com>
2022-01-30 13:55:43 +0300

    fix name change of script file

---
+++ play_wordle.R
@@ -0,0 +1,22 @@
+source("wordle_functions.R")
+
+reset()  
+prep_words()
+
+# For the first round of words
+get_words() 
+
+### sum_pos_props = sum of all the probabilities that the letter will appear in its 
+    ### particular position in the subset of feasible words
+### sum_prop_words = sum of all the probabilities that the letters will appear anywhere
+    ### in the subset of feasible words.
+### repeat_ltrs = number of letters repeated in the word. Better to use 0 early on.
+
+### write the word you entered in "letters"
+### and the color of the results in "colors" where:
+###      green = 'g', black = 'b', yellow = 'y'
+run_attempt(letters_attempt = "daddy", colors_attempt = "ggbyb")
+
+# Rerun it for subsequent attempts. If you need to start over, just run reset() and prep_words()
+
+
--- wordle_functions.R
+++ wordle_functions.R
@@ -15,11 +15,15 @@ reset <- function(){
   not_pos_3 <<- NA
   not_pos_4 <<- NA
   not_pos_5 <<- NA
+  only_1 <<- NA
+  only_2_plus <<- NA
+  only_2 <<- NA
+  only_3 <<- NA
   
 }
 
 prep_words <- function(){  
-  five_ltr_words <- words::words %>% 
+  five_ltr_words <<- words::words %>% 
     filter(word_length==5) %>% 
     mutate(`1` = str_sub(word,1,1),
            `2` = str_sub(word,2,2),
@@ -28,24 +32,59 @@ prep_words <- function(){
            `5` = str_sub(word,5,5)) %>% 
     pivot_longer(cols = `1`:`5`, names_to = "position", values_to = "letter") 
   
-  repeat_letter_words <- five_ltr_words %>% 
-    count(word,letter) %>% 
-    filter(n > 1) %>% 
-    select(word) %>% 
-    distinct() %>% 
+  repeat_letter_words <- five_ltr_words %>%
+    count(word,letter) %>%
+    filter(n > 1) %>%
+    select(word) %>%
+    distinct() %>%
     pull()
   
+  word_set <<- five_ltr_words
   five_ltr_words_clean <<- five_ltr_words %>% 
     filter(!word %in% repeat_letter_words) 
 }
 
-get_filtered_words <- function(letters_in = NA, 
-                   letters_out = NA, 
-                   pos_1 = NA, pos_2 = NA, pos_3 = NA,pos_4 = NA,pos_5 = NA,
-                   not_pos_1 = NA, not_pos_2 = NA, not_pos_3 = NA,not_pos_4 = NA,not_pos_5 = NA){
-  
-  out<- 
-    five_ltr_words_clean %>% 
+filter_duplicates <- function(dup_letter='o',type="only_1"){
+  ltr_count <- word_set %>% 
+    count(word,letter) 
+  if(type=="only_1"){
+    filtered_words <- 
+      ltr_count %>% 
+      filter(letter==dup_letter & n==1) %>% 
+      pull(word)
+    out <- word_set %>% 
+      filter(word %in% filtered_words)
+  }
+  if(type=="only_2"){
+    filtered_words <- 
+      ltr_count %>% 
+      filter(letter==dup_letter & n==2) %>% 
+      pull(word)
+    out <- word_set %>% 
+      filter(word %in% filtered_words)
+  }
+  if(type=="only_2_plus"){
+    filtered_words <- 
+      ltr_count %>% 
+      filter(letter==dup_letter & n>=2) %>% 
+      pull(word)
+    out <- word_set %>% 
+      filter(word %in% filtered_words)
+  }
+  if(type=="only_3"){
+    filtered_words <- 
+      ltr_count %>% 
+      filter(letter==dup_letter & n==3) %>% 
+      pull(word)
+    out <- word_set %>% 
+      filter(word %in% filtered_words)
+  }
+  word_set <<- out
+}
+
+get_filtered_words <- function(){  
+  word_set <<- 
+    word_set %>% 
     pivot_wider(names_from = 'position',values_from="letter") %>% 
     filter(is.na(pos_1)[1] | `1` == pos_1,
            is.na(pos_2)[1] | `2` == pos_2, 
@@ -60,10 +99,12 @@ get_filtered_words <- function(letters_in = NA,
     filter(is.na(letters_in)[1] | str_detect(word,str_c("(?=.*",str_c(letters_in, collapse =")(?=.*"),")"))) %>% #"(?=.*p)(?=.*a)(?=.*e)"
     filter(is.na(letters_out)[1] | !(str_detect(word,str_c("[",str_c(letters_out,collapse= ""),"]")))) %>% 
     pivot_longer(cols = c(`1`,`2`,`3`,`4`,`5`),names_to ="position",values_to = "letter")
-  
-  return(out)
+    
+  if(!is.na(only_1)) map(.x=only_1,filter_duplicates,type="only_1")
+  if(!is.na(only_2)) map(.x=only_2,filter_duplicates,type="only_2")
+  if(!is.na(only_2_plus)) map(.x=only_2_plus,filter_duplicates,type="only_2_plus")
+  if(!is.na(only_3)) map(.x=only_3,filter_duplicates,type="only_3")
 }
-
 
 get_stats <- function(data = data){
   letter_stats <- data %>% 
@@ -84,11 +125,12 @@ get_stats <- function(data = data){
   return(letter_position_stats)
 }
 
-get_words <- function(new_words=new_words){
+get_words <- function(){
   
-  new_stats <- get_stats(new_words)
+  new_stats <- get_stats(word_set)
   
-  out <- new_words %>% 
+  out <- word_set %>% 
+    # count(word,letter,name="ltr_count")
     pivot_wider(names_from = 'position',values_from="letter") %>% 
     mutate(prop_words = NA) %>% 
     left_join(new_stats %>% select(letter,`1`,prop_words),by=c(`1`="letter")) %>%  
@@ -96,10 +138,16 @@ get_words <- function(new_words=new_words){
     left_join(new_stats %>% select(letter,`3`,prop_words),by=c(`3`="letter")) %>% 
     left_join(new_stats %>% select(letter,`4`,prop_words),by=c(`4`="letter")) %>% 
     left_join(new_stats %>% select(letter,`5`,prop_words),by=c(`5`="letter")) %>% 
+    left_join(word_set %>% 
+                count(word,letter,name="ltr_count") %>% 
+                filter(ltr_count>1) %>% 
+                group_by(word) %>% 
+                summarize(repeat_ltrs = length(ltr_count))) %>%
+    replace_na(list(repeat_ltrs = 0)) %>% 
     mutate(sum_pos_props = `1.y` + `2.y` + `3.y` + `4.y` + `5.y`,
            sum_prop_words = prop_words.y + prop_words.y.y + prop_words.y.y.y + prop_words.x.x + prop_words.x.x.x) %>%
     arrange(desc(sum_pos_props)) %>% 
-    select(word,sum_pos_props,sum_prop_words)
+    select(word,sum_pos_props,sum_prop_words,repeat_ltrs)
     return(out)
     
 }
@@ -133,15 +181,60 @@ letter_color_check <- function(index=4) {
   }
 }
 
+check_duplicates <- function(dup_letter = "d"){
+  ltr_break <- letters_attempt %>% 
+    str_split("") %>% 
+    unlist() 
+  indices <- which(ltr_break==dup_letter)
+  clr_break <- colors_attempt %>% 
+    str_split("") %>% 
+    unlist()
+  num_pos_in <- sum(clr_break[indices]%in% c("g", "y"))
+  if(num_pos_in==1) {
+    if(is.na(only_1)[1]){ 
+      only_1 <<- dup_letter
+    } else { only_1 <<- c(only_1,dup_letter) }
+  }
+  if(num_pos_in==2 & length(indices)>2) {
+    if(is.na(only_2)[1]){ 
+      only_2 <<- dup_letter
+    } else{ only_2 <<- c(only_2,dup_letter) }
+  }
+  if(num_pos_in==2 & length(indices)==2) {
+    if(is.na(only_2_plus)[1]){ 
+      only_2_plus <<- dup_letter
+    } else{ only_2_plus <<- c(only_2_plus,dup_letter) }
+  }
+  if(num_pos_in==3) {
+    if(is.na(only_3)[1]){ 
+      only_3 <<- dup_letter
+    } else{ only_3 <<- c(only_3,dup_letter) }
+  }
+  if(num_pos_in!=0){  
+    for(i in indices){
+      if(clr_break[i]=="b") clr_break[i]="y"
+    }
+  }
+  colors_attempt <<- str_c(clr_break,collapse="")
+}
+
+
 run_attempt <- function(letters_attempt = letters_attempt,colors_attempt=colors_attempt){
   letters_attempt <<- letters_attempt
   colors_attempt <<- colors_attempt
-  purrr::map(.x = 1:5,letter_color_check)
+  dup_letters <- letters_attempt %>% 
+    str_split("") %>% 
+    unlist() %>% 
+    tibble(ltrs = .) %>% 
+    count(ltrs) %>% 
+    filter(n>1) %>% 
+    pull(ltrs)
   
-  get_words(get_filtered_words(letters_in = letters_in,
-                               letters_out = letters_out, 
-                               pos_1 = pos_1, pos_2 = pos_2, pos_3 = pos_3,pos_4 = pos_4,pos_5 = pos_5,
-                               not_pos_1 = not_pos_1, not_pos_2 = not_pos_2, not_pos_3 = not_pos_3,not_pos_4 = not_pos_4,not_pos_5 = not_pos_5)
-  )
+  map(.x=dup_letters,.f = check_duplicates)
+  
+  purrr::map(.x = 1:5,letter_color_check)
+  get_filtered_words()
+  get_words()
+
 }
 
